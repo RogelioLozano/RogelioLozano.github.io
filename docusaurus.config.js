4f40f1e4f88f86eaf53d20753652ebd12e83c807
Rogelio Lozano <rogelio.lozanoa@sat.gob.mx>
2024-03-06 03:49:03 -0600

    added custom css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Docs to connect the dots!',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Only through time time is conquered.',
   favicon: 'static/img/favicon.ico',
 
   // Set the production url of your site here
